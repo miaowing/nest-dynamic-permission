419b188410278deea1bb9630e5f80ffd720afd72
miaowing <i@zfeng.net>
2018-07-23 18:15:32 +0800

    support init with boot module

--- lib/permission.module.ts
+++ lib/permission.module.ts
@@ -1,15 +1,32 @@
 import { Module, DynamicModule, Global } from '@nestjs/common';
 import { DynamicPermission } from './dynamic.permisson';
+import { Boot } from 'nest-boot';
 
 @Global()
 @Module({})
 export class PermissionModule {
-  static forRoot(controllers: any, service?: string): DynamicModule {
+  static init(controllers: any, service?: string): DynamicModule {
     const permissionProvider = {
       provide: 'DynamicPermission',
       useFactory: (): DynamicPermission => {
         return new DynamicPermission(controllers, service);
       },
+    };
+
+    return {
+      module: PermissionModule,
+      components: [permissionProvider],
+      exports: [permissionProvider],
+    };
+  }
+
+  static initWithBoot(controllers: any, path: string): DynamicModule {
+    const permissionProvider = {
+      provide: 'DynamicPermission',
+      useFactory: (boot: Boot): DynamicPermission => {
+        return new DynamicPermission(controllers, boot.get(path));
+      },
+      inject: ['BootstrapProvider'],
     };
 
     return {
